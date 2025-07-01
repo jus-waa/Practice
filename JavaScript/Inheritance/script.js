7999b18b3ceae3da82fd8ee1f642dba7ff80aa67
josh <joshlagrimas1808@gmail.com>
2025-07-02 01:47:13 +0800

    sample inheritance

--- JavaScript/Inheritance/script.js
+++ JavaScript/Inheritance/script.js
@@ -1,1 +1,17 @@
-tests
+class Person {
+    walk() {
+        return "Walking";
+    }
+}
+const me = new Person();
+console.log(me.walk());
+
+class Bird extends Person{
+    fly() {
+        return  "Flying";
+    }
+}
+
+const maya = new Bird(); 
+
+console.log(maya.walk() + maya.fly());
